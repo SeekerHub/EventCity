dd9bc26a115a27ed1b384874a72f59f563bbf453
Bhavesh <bhavesh@Bhaveshs-MacBook-Air.local>
2025-10-20 18:28:53 +0530

    Upgraded versions

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,54 +1,42 @@
-import { Box, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { getAllMovies } from "../../api-helpers/api-helpers";
 import MovieItem from "./MovieItem";
-import CircularProgress from '@mui/material/CircularProgress';
+import "./Movies.css";
 
 const Movies = () => {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
   const [events, setEvents] = useState([]);
   const [loader, setLoader] = useState(true);
+
   useEffect(() => {
     getAllMovies()
       .then((data) => {
         let movi = [];
         let even = [];
         data.movies.forEach(obj => {
-          if(obj["isEvent"]==false){
+          if(obj["isEvent"] === false){
             movi.push(obj);
           }else{
             even.push(obj);
           }
-        })
+        });
         setMovies(movi);
         setEvents(even);
         setLoader(false);
       })
       .catch((err) => console.log(err));
   }, []);
+
   return (
-    <Box margin={"auto"} marginTop={4}>
-      <Typography
-        margin={"auto"}
-        variant="h5"
-        padding={2}
-        width="40%"
-        bgcolor={"#900C3F"}
-        color="white"
-        align="center"
-        textAlign={"center"}
-      >
-        All Movies
-      </Typography>
-      <Box
-        width={"100%"}
-        margin="auto"
-        marginTop={5}
-        display={"flex"}
-        justifyContent="center"
-        flexWrap={"wrap"}
-      >
-        {loader?  <CircularProgress disableShrink />:movies &&
+    <div className="movies-root">
+      <div className="movies-section">
+        <h2 className="movies-title">All Movies</h2>
+      </div>
+      <div className="movies-list">
+        {loader ? (
+          <div className="movies-loader"></div>
+        ) : (
+          movies &&
           movies.map((movie, index) => (
             <MovieItem
               key={index}
@@ -57,29 +45,17 @@ const Movies = () => {
               releaseDate={movie.releaseDate}
               title={movie.title}
             />
-          ))}
-      </Box>
-
-      <Typography
-        margin={"auto"}
-        variant="h5"
-        padding={2}
-        width="40%"
-        bgcolor={"#900C3F"}
-        color="white"
-        textAlign={"center"}
-      >
-        All Events
-      </Typography>
-      <Box
-        width={"100%"}
-        margin="auto"
-        marginTop={5}
-        display={"flex"}
-        justifyContent="center"
-        flexWrap={"wrap"}
-      >
-        {loader?  <CircularProgress disableShrink />:events &&
+          ))
+        )}
+      </div>
+      <div className="movies-section">
+        <h2 className="movies-title">All Events</h2>
+      </div>
+      <div className="movies-list">
+        {loader ? (
+          <div className="movies-loader"></div>
+        ) : (
+          events &&
           events.map((event, index) => (
             <MovieItem
               key={index}
@@ -88,9 +64,10 @@ const Movies = () => {
               releaseDate={event.releaseDate}
               title={event.title}
             />
-          ))}
-      </Box>
-    </Box>
+          ))
+        )}
+      </div>
+    </div>
   );
 };
 
